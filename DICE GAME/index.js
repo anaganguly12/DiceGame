29d2c8e48e1e4dba7acd76fdd63ed776030efc95
Ananya Ganguly <78080646+ananyaganguly@users.noreply.github.com>
2022-06-07 17:23:03 +0530

    Add files via upload

---
+++ DICE GAME/index.js
@@ -0,0 +1,30 @@
+var randomNum1 = Math.floor(Math.random() * 6 ) + 1;
+
+var randomDiceImg1 = "dice" + randomNum1 + ".png";
+
+var randomImgSrc1 = "images/" + randomDiceImg1;
+
+var img1 = document.querySelectorAll("img")[0];
+
+img1.setAttribute("src", randomImgSrc1);
+
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg2 = "dice" + randomNum2 + ".png";
+
+var randomImgSrc2 = "images/" + randomDiceImg2;
+
+var img2 = document.querySelectorAll("img")[1];
+
+img2.setAttribute("src", randomImgSrc2);
+
+if(randomNum1 > randomNum2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!🚩";
+}
+else if(randomNum2 > randomNum1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
